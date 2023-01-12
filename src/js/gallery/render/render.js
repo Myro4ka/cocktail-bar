4c1da3df8ae28b2bbd361912802ccf45362776a0
Myro4ka <www.mirosya@gmail.com>
2023-01-12 17:07:23 +0100

    add js for favorites

--- src/js/gallery/render/render.js
+++ src/js/gallery/render/render.js
@@ -1,9 +1,10 @@
+import sprite from '../../../images/symbol-defs.svg';
+
 export const coctailCardMarkup = (
   markupPlace = '',
   cocktailName = '',
   cocktailImgLink = '',
-  cocktailId = '',
-  coctailNumber = 0,
+  cocktailId = ''
 ) => {
   markupPlace.innerHTML += `<li class="gallery__item">
                 <img class="gallery__img" src="${cocktailImgLink}" alt="${cocktailName}" width="280" height="280" />
@@ -11,13 +12,13 @@ export const coctailCardMarkup = (
                     <h3 class="gallery__card-title">${cocktailName}</h3>
                     <div class="gallery__card-btns">
                         <button class="btn__learn coctails-section__learn-button" type="button" data-cocktailId = ${cocktailId}>Learn more</button>
-                        <button class="btn__add coctails-section__like-button" type="button" id="likeBtn${coctailNumber}">
+                        <button class="btn__add coctails-section__like-button" type="button">
                             Add to
                             <svg class="btn__svg" width="18" height="18">
-                                <use href="./images/symbol-defs.svg#icon-Heart-mobile"></use>
+                                <use href="${sprite}#icon-Heart-mobile"></use>
                             </svg>
                         </button>
                     </div>
                 </div>
-            </li> `
-}
+            </li> `;
+};
