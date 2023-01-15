bad7757ae75cf015fbf275edb1a3afd50627c8f1
Myro4ka <www.mirosya@gmail.com>
2023-01-15 14:26:22 +0100

    fixed header

--- src/js/gallery/refs/refs.js
+++ src/js/gallery/refs/refs.js
@@ -3,11 +3,6 @@ export const refs = {
   coctailTitel: document.querySelector('.gallery__title'),
   coctailsList: document.querySelector('.gallery__list'),
   coctailContainer: document.querySelectorAll('.coctail-container'),
-  learnBtn: document.querySelector('.coctails-section__learn-button'),
 
-  addLikeBtn: document.querySelector('.coctails-section__like-button'),
   paginationDiv: document.querySelector('.pagination'),
-
-  learmMoreBtn: document.querySelector('.btn__learn'),
-  cardButtonsWrap: document.querySelector('.gallery__card-btns'),
 };
